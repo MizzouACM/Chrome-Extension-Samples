dade6a044f6813d09c044d5e985fe103c98b4ed3
Dan Silver <das2c3@mail.missouri.edu>
2013-04-29 12:23:35 -0500

    Replaced alert() with desktop notification

--- FinishedExtensions/PirateTranslator/background.js
+++ FinishedExtensions/PirateTranslator/background.js
@@ -1,7 +1,13 @@
 $(function() {
   function onClick(info, tab) {
     $.get('https://isithackday.com/arrpi.php', {text: info.selectionText}, function(res) {
-      alert(res)
+      // Create a simple text notification:
+		var notification = webkitNotifications.createNotification(
+		  'pirate_32.png',  // icon url - can be relative
+		  'Pirate Translator!',  // notification title
+		  res  // notification body text
+		);
+		notification.show()
     })
   }
   chrome.contextMenus.create({"title": "Translate into pirate!", "contexts":["selection"], "onclick": onClick})
